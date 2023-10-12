87a6a5f770b8a1a99c90c00043de52b532b95778
Bocaletto Luca <134635227+elektronoide@users.noreply.github.com>
2023-10-12 20:28:09 +0200

    Add files via upload

---
+++ Tris2Player_English.py
@@ -0,0 +1,106 @@
+# Software Name: Tris 2 Player
+# Author: Luca Bocaletto
+
+# Import the Tkinter library for GUI and messagebox for message pop-ups.
+import tkinter as tk
+from tkinter import messagebox
+
+# Main class for the Tic-Tac-Toe game.
+class TrisGame:
+    # Class variables to keep track of player wins.
+    player1_wins = 0
+    player2_wins = 0
+
+    def __init__(self):
+        # Initialize the game.
+        self.current_player = "X"  # Start with player X.
+        self.board = [" " for _ in range(9)]  # Initialize a list for the empty grid.
+        self.buttons = []  # List of buttons on the grid.
+
+    def setup_window(self):
+        # Set up the game window.
+        self.window = tk.Tk()  # Create a Tkinter window.
+        self.window.title("Tic-Tac-Toe")  # Set the window title.
+
+        # Labels to display player scores.
+        self.label_player1 = tk.Label(self.window, text=f"Player 1 (X) Wins: {TrisGame.player1_wins}")
+        self.label_player1.grid(row=3, column=0, columnspan=3)
+
+        self.label_player2 = tk.Label(self.window, text=f"Player 2 (O) Wins: {TrisGame.player2_wins}")
+        self.label_player2.grid(row=4, column=0, columnspan=3)
+
+        # Create the 3x3 grid of buttons for the game.
+        for i in range(3):
+            row = []
+            for j in range(3):
+                # Create a button with empty text.
+                button = tk.Button(self.window, text=" ", font=("normal", 20), width=10, height=2,
+                                   command=lambda row=i, col=j: self.on_button_click(row, col))
+                button.grid(row=i, column=j)  # Place the button on the grid.
+                row.append(button)  # Add the button to the list of buttons.
+            self.buttons.append(row)  # Add the row of buttons to the list of buttons.
+
+    def on_button_click(self, row, col):
+        # Handle the button click event.
+        if self.board[row * 3 + col] == " ":
+            self.board[row * 3 + col] = self.current_player  # Update the grid with the current player's symbol.
+            text_color = "red" if self.current_player == "X" else "blue"  # Set text color.
+            self.buttons[row][col].config(text=self.current_player, fg=text_color)  # Update the button.
+
+            if self.check_winner(row, col):
+                # Check if the player has won.
+                messagebox.showinfo("Win!", f"Player {self.current_player} has won!")
+                if self.current_player == "X":
+                    TrisGame.player1_wins += 1  # Update the score for player X.
+                else:
+                    TrisGame.player2_wins += 1  # Update the score for player O.
+                self.label_player1.config(text=f"Player 1 (X) Wins: {TrisGame.player1_wins}")
+                self.label_player2.config(text=f"Player 2 (O) Wins: {TrisGame.player2_wins}")
+                self.reset_board()  # Restart the game.
+            else:
+                if " " not in self.board:
+                    # If there are no empty spaces on the grid, the game is a tie.
+                    messagebox.showinfo("Tie", "The game ended in a tie.")
+                    self.reset_board()  # Restart the game.
+                else:
+                    self.current_player = "O" if self.current_player == "X" else "X"  # Switch to the next player.
+
+    def check_winner(self, row, col):
+        # Check if the current player has won.
+        directions = [(0, 1), (1, 0), (1, 1), (1, -1)]
+        for dr, dc in directions:
+            count = 1
+            for i in range(1, 3):
+                r = row + dr * i
+                c = col + dc * i
+                if 0 <= r < 3 and 0 <= c < 3 and self.board[r * 3 + c] == self.current_player:
+                    count += 1
+                else:
+                    break
+            for i in range(1, 3):
+                r = row - dr * i
+                c = col - dc * i
+                if 0 <= r < 3 and 0 <= c < 3 and self.board[r * 3 + c] == self.current_player:
+                    count += 1
+                else:
+                    break
+            if count >= 3:
+                return True  # The player has won.
+
+        return False  # The player has not won.
+
+    def reset_board(self):
+        # Restart the game by closing the current window and creating a new instance.
+        self.window.destroy()  # Close the current window.
+        game = TrisGame()  # Create a new instance of TrisGame.
+        game.run()  # Start the new game.
+
+    def run(self):
+        # Start the game.
+        self.setup_window()  # Set up the window.
+        self.window.mainloop()  # Start the main Tkinter loop.
+
+if __name__ == "__main__":
+    # Run the game when the module is executed as the main script.
+    game = TrisGame()  # Create an instance of the game.
+    game.run()  # Start the game.
---
+++ Tris2Player_Italian.py
@@ -0,0 +1,106 @@
+# Nome del software: Tris 2 Player
+# Author: Bocaletto Luca
+
+# Importa la libreria Tkinter per la GUI e messagebox per i popup di messaggi.
+import tkinter as tk
+from tkinter import messagebox
+
+# Classe principale per il gioco del Tris.
+class TrisGame:
+    # Variabili di classe per tenere traccia delle vittorie dei giocatori.
+    player1_wins = 0
+    player2_wins = 0
+
+    def __init__(self):
+        # Inizializza il gioco.
+        self.current_player = "X"  # Inizia con il giocatore X.
+        self.board = [" " for _ in range(9)]  # Inizializza una lista per la griglia vuota.
+        self.buttons = []  # Lista di pulsanti sulla griglia.
+
+    def setup_window(self):
+        # Configura la finestra del gioco.
+        self.window = tk.Tk()  # Crea una finestra Tkinter.
+        self.window.title("Tris")  # Imposta il titolo della finestra.
+
+        # Etichette per visualizzare il punteggio dei giocatori.
+        self.label_player1 = tk.Label(self.window, text=f"Player 1 (X) Vittorie: {TrisGame.player1_wins}")
+        self.label_player1.grid(row=3, column=0, columnspan=3)
+
+        self.label_player2 = tk.Label(self.window, text=f"Player 2 (O) Vittorie: {TrisGame.player2_wins}")
+        self.label_player2.grid(row=4, column=0, columnspan=3)
+
+        # Crea la griglia di bottoni 3x3 per il gioco.
+        for i in range(3):
+            row = []
+            for j in range(3):
+                # Crea un pulsante con testo vuoto.
+                button = tk.Button(self.window, text=" ", font=("normal", 20), width=10, height=2,
+                                   command=lambda row=i, col=j: self.on_button_click(row, col))
+                button.grid(row=i, column=j)  # Posiziona il pulsante sulla griglia.
+                row.append(button)  # Aggiunge il pulsante alla lista dei pulsanti.
+            self.buttons.append(row)  # Aggiunge la riga di pulsanti alla lista dei pulsanti.
+
+    def on_button_click(self, row, col):
+        # Gestisce l'evento di clic su un pulsante.
+        if self.board[row * 3 + col] == " ":
+            self.board[row * 3 + col] = self.current_player  # Aggiorna la griglia con il simbolo del giocatore corrente.
+            text_color = "red" if self.current_player == "X" else "blue"  # Imposta il colore del testo.
+            self.buttons[row][col].config(text=self.current_player, fg=text_color)  # Aggiorna il pulsante.
+
+            if self.check_winner(row, col):
+                # Verifica se il giocatore ha vinto.
+                messagebox.showinfo("Vittoria!", f"Il giocatore {self.current_player} ha vinto!")
+                if self.current_player == "X":
+                    TrisGame.player1_wins += 1  # Aggiorna il punteggio del giocatore X.
+                else:
+                    TrisGame.player2_wins += 1  # Aggiorna il punteggio del giocatore O.
+                self.label_player1.config(text=f"Player 1 (X) Vittorie: {TrisGame.player1_wins}")
+                self.label_player2.config(text=f"Player 2 (O) Vittorie: {TrisGame.player2_wins}")
+                self.reset_board()  # Riavvia il gioco.
+            else:
+                if " " not in self.board:
+                    # Se non ci sono spazi vuoti sulla griglia, il gioco è un pareggio.
+                    messagebox.showinfo("Pareggio", "Il gioco è finito in pareggio.")
+                    self.reset_board()  # Riavvia il gioco.
+                else:
+                    self.current_player = "O" if self.current_player == "X" else "X"  # Passa al prossimo giocatore.
+
+    def check_winner(self, row, col):
+        # Controlla se il giocatore corrente ha vinto.
+        directions = [(0, 1), (1, 0), (1, 1), (1, -1)]
+        for dr, dc in directions:
+            count = 1
+            for i in range(1, 3):
+                r = row + dr * i
+                c = col + dc * i
+                if 0 <= r < 3 and 0 <= c < 3 and self.board[r * 3 + c] == self.current_player:
+                    count += 1
+                else:
+                    break
+            for i in range(1, 3):
+                r = row - dr * i
+                c = col - dc * i
+                if 0 <= r < 3 and 0 <= c < 3 and self.board[r * 3 + c] == self.current_player:
+                    count += 1
+                else:
+                    break
+            if count >= 3:
+                return True  # Il giocatore ha vinto.
+
+        return False  # Il giocatore non ha vinto.
+
+    def reset_board(self):
+        # Riavvia il gioco chiudendo la finestra attuale e creandone una nuova istanza.
+        self.window.destroy()  # Chiude la finestra attuale.
+        game = TrisGame()  # Crea una nuova istanza di TrisGame.
+        game.run()  # Avvia il nuovo gioco.
+
+    def run(self):
+        # Avvia il gioco.
+        self.setup_window()  # Configura la finestra.
+        self.window.mainloop()  # Avvia il ciclo principale Tkinter.
+
+if __name__ == "__main__":
+    # Esegue il gioco quando il modulo viene eseguito come script principale.
+    game = TrisGame()  # Crea un'istanza del gioco.
+    game.run()  # Avvia il gioco.
